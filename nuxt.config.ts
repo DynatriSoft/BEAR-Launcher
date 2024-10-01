ae604364dea666a00ec83841b05f667ec86c164b
André Nunes <hello@anuness.dev>
2024-10-01 23:40:14 +0100

    Setup

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  ssr: false,
+  devServer: { host: process.env.TAURI_DEV_HOST || 'localhost' },
+  vite: {
+    clearScreen: false,
+    envPrefix: ['VITE_', 'TAURI_'],
+    server: {
+      strictPort: true,
+    },
+  },
+})
